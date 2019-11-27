d82ae0c34a395491bdbc5e8a11646721415f4da3
ChristianH321 <55371812+ChristianH321@users.noreply.github.com>
2019-11-27 12:48:04 +0100

    Version 10
    working on BE's - Setters and getters and Instance variables

--- MyTunes/src/be/Playlist.java
+++ MyTunes/src/be/Playlist.java
@@ -5,10 +5,130 @@
  */
 package be;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author chris
  */
 public class Playlist {
+    
+    private int id;
+    private String name;
+    private int numberOfSongs;
+    private int totalLength;
+    private List<Song> tracklist;
+    
+    /**
+     * Constructs a new playlist without any songs.
+     * 
+     * @param id The id of playlist
+     * @param name The name of playlist
+     */
+    
+    public Playlist(int id, String name) {
+        
+        this.id = id;
+        this.name = name;
+        totalLength = 0;
+        numberOfSongs = 0;
+        tracklist = new ArrayList();
+        
+    }
+    
+    /**
+     * Adds song to playlist.
+     * 
+     * @param song The song to add.
+     */
+    
+    public void addSong(Song song) {
+        
+        tracklist.add(song);
+        totalLength += song.getLength();
+        numberOfSongs++;
+        
+    }
+    
+    /**
+     * Removes song from playlist.
+     * 
+     * @param song The song to remove.
+     */
+    
+    public void removeSong(Song song) {
+        
+        tracklist.remove(song);
+        totalLength -= song.getLength();
+        numberOfSongs--;
+        
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNumberOfSongs() {
+        return numberOfSongs;
+    }
+
+    public int getTotalLength() {
+        return totalLength;
+    }
+
+    public List<Song> getTracklist() {
+        return tracklist;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setTracklist(List<Song> tracklist) {
+        this.tracklist = tracklist;
+    }
+    /**
+     * Returns total amount of time of all songs on playlist
+     * in format specified by {@ TimeConverter} class.
+     * 
+     * @return The formatted time.
+     */
+    public String getTotalLengthInString() {
+        return TimeConverter.convertToString(totalLength);
+    }
+    
+    /**
+     * Returns the position of song on playlist. If
+     * given song is not on playlist, returns -1.
+     * 
+     * @param song the Song from playlist.
+     * @return The position of song on Playlist.
+     */
+    public int getPositionOfSong(Song song) {
+        return tracklist.indexOf(song);
+    }
+    
+    /**
+     * Returns true if given song is on the playlist.
+     * 
+     * @param song The song to check.
+     * @param true if song is on playlist.
+     */
+    
+    public boolean isSongOnTracklist(Song song) {
+        for(Song s : tracklist) {
+            if(s.getId() == song.getId()) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    
     
 }
--- MyTunes/src/be/Song.java
+++ MyTunes/src/be/Song.java
@@ -11,4 +11,89 @@ package be;
  */
 public class Song {
     
+    private int id;
+    private String title;
+    private String artist;
+    private String category;
+    private int length;
+    private String path;
+    
+    /**
+     * Constructs a new song
+     * 
+     * @param id the ID of the song
+     * @param title the title of the song
+     * @param artist the artist of the song
+     * @param category the genre of the song
+     * @param length the length of the song
+     * @param path the path of the song
+     */
+    
+    public Song(int id, String title, String artist, String category, int length, String path) {
+        
+        this.id = id;
+        this.title = title;
+        this.artist = artist;
+        this.category = category;
+        this.length = length;
+        this.path = path;
+        
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getLength() {
+        return length;
+    }
+    
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+    /**
+     * Returns in format specified by {@code TimeConverter} class.
+     * 
+     * @return The formatted time.
+     */
+    public String getLengthInString() {
+        return TimeConverter.convertToString(length);
+    }
+    
+    @Override
+    public String toString() {
+        return title;
+    }
+
+
+    
+    
 }

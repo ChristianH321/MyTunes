752a716fa76f80f5b06d39aa269d1164aa9d6f5b
ChristianH321 <55371812+ChristianH321@users.noreply.github.com>
2019-11-28 10:12:47 +0100

    Version 13
    Created connection to Database
    changed the fx:controller

--- MyTunes/src/dal/DbConnectionProvider.java
+++ MyTunes/src/dal/DbConnectionProvider.java
@@ -5,10 +5,56 @@
  */
 package dal;
 
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+import com.microsoft.sqlserver.jdbc.SQLServerException;
+import com.sun.jdi.connect.spi.Connection;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 /**
+ * The {@code DbConnectionProvider} class is responsible for
+ * establishing connection with a database.
  *
  * @author chris
  */
 public class DbConnectionProvider {
     
+    private static final String PROP_FILE = "data/connectionInfo.settings";
+    private SQLServerDataSource ds;
+    
+    /**
+     * creates a connection with the database.
+     */
+    
+    public DbConnectionProvider() {
+        
+        try {
+            
+            Properties databaseProperties = new Properties();
+            databaseProperties.load(new FileInputStream(PROP_FILE));
+            ds = new SQLServerDataSource();
+            ds.setServerName(databaseProperties.getProperty("10.176.111.31"));
+            ds.setDatabaseName(databaseProperties.getProperty("ProjectMyTunesDB"));
+            ds.setUser(databaseProperties.getProperty("CSe19B_17"));
+            ds.setPassword(databaseProperties.getProperty("CSe19B_17"));
+        }
+        catch(IOException e) {
+            
+            // To Do
+        }
+    }
+    
+    /**
+     * Returns Connection object which is able to
+     * provide informations about database.
+     * 
+     * @return The Connection with database.
+     * @throws SQLServerException if connection with database cannot be established.
+     */
+    
+    public Connection getConnection() throws SQLServerException {
+        
+        return (Connection) ds.getConnection();
+    }
 }
